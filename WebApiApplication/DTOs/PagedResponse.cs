4ebc04e35d37ac6dac24795ad4c06417877af501
Ondřej Bajer <bajer.o@centrum.cz>
2026-02-15 11:54:05 +0100

    feat: introduce pagination model and extend service contract

---
+++ WebApiApplication/DTOs/PagedResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApiApplication.DTOs
+{
+    public sealed record PagedResponse<T>
+    (
+    IReadOnlyList<T> Items,
+    int Page,
+    int PageSize,
+    int TotalCount)
+    {
+        public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
+    }
+}
--- WebApiApplication/Interfaces/IProductService.cs
+++ WebApiApplication/Interfaces/IProductService.cs
@@ -5,6 +5,7 @@ namespace WebApiApplication.Interfaces
     public interface IProductService
     {
         IReadOnlyList<ProductDto> GetAll();
+        PagedResponse<ProductDto> GetPaged(int page, int pageSize);
         ProductDto? GetById(int id);
         bool UpdateDescription(int id, string? description);
     }
